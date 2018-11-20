a94dc1def9b3f0ee3d5eb56e592b69217bdd387b
Edoardo Comar <ecomar@uk.ibm.com>
2018-11-20 15:16:16 +0000

    comment

--- core/src/main/scala/kafka/server/KafkaApis.scala
+++ core/src/main/scala/kafka/server/KafkaApis.scala
@@ -2237,7 +2237,7 @@ class KafkaApis(val requestChannel: RequestChannel,
     request.temporaryMemoryBytes = conversionStats.temporaryMemoryBytes
   }
 
-  private def handleError(request: RequestChannel.Request, e: Throwable) { //EDO maybe ProduceWithOffset should not be a clusterAction to be throtteable?
+  private def handleError(request: RequestChannel.Request, e: Throwable) {
     val mayThrottle = e.isInstanceOf[ClusterAuthorizationException] || !request.header.apiKey.clusterAction
     error("Error when handling request: " +
       s"clientId=${request.header.clientId}, " +
